da78a09b85a4d47a02ac2daed9b640d29bf906c9
Patrick Henn <patrickhenn@Air-von-Patrick.fritz.box>
2021-04-01 16:27:09 +0200

    create

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const request = require('request-promise-native');
 require('dotenv').config();
 
-async function post() {
+const orgaId = process.env.ODEV;
+const token = process.env.DEV
+
+async function createOrder() {
+    const y = await request ({
+        method: 'POST',
+        uri: 'https://api.dev.nx.bezahl.de/nxt/v1/order/',
+        headers: {
+            'Content-Type': 'application/json',
+            'NX-Token': token,
+        },
+        json: true,
+        body: {
+            orgId: `${orgaId}`,
+            name: 'Glück',
+            price: 7777,
+            recipient: 'patrick.henn+test@nx-technologies.com',
+        }  
+    });
+    console.log('1');
+    return y
+}
+
+async function sendOrder() {
     const x = await request ({
         method: 'POST',
         uri: 'http://localhost:5000/posts',
         title : 'Wetter',
         content : 'tolles Wetter',
-        // body:
-        // {
-        //     title : 'Wetter',
-        //     content : 'tolles Wetter'
-        // }
+        body: {
+            title : 'Wetter',
+            content : 'tolles Wetter'
+        }
     });
-    console.log('1');
+    console.log('2');
+    return x
 }
 
-post().then(console.log)
+sendOrder().then(console.log);
+createOrder().then(console.log);
 
 // header:
 // {
